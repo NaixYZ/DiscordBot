014bc9c0783ce5b22800e38e8b2a54b3605ac33d
Naix <76706715+NaixYZ@users.noreply.github.com>
2025-02-28 10:40:26 +0100

    Update reqirements.bat

--- reqirements.bat
+++ reqirements.bat
@@ -1,16 +1,7 @@
 @echo off
 
-:: Erstelle eine virtuelle Umgebung
-python -m venv venv
+pip install discord.py
 
-:: Aktiviere die virtuelle Umgebung
-call venv\Scripts\activate
-
-:: Installiere die benötigten Pakete
-pip install -U discord.py
-
-:: Starte den Bot
 python main.py
 
-:: Warte auf Benutzereingabe, um das Fenster offen zu halten
 pause
